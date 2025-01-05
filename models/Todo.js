5489b3e01a1b9290cd222cc4a03b2cfce8b00fd6
Ayush <ayushsaxena917@gmail.com>
2025-01-05 22:39:45 +0530

    Adding userid field to Todo Schema

--- models/Todo.js
+++ models/Todo.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
-  todo: { type: String, required: true },
-  completed: { type: String, required: true },
+  todo: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("TodoCollection", TodoSchema);
